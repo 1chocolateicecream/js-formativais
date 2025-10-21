1b265c020a30128aacba9b98e9c0516c20e98c03
Your Name <you@example.com>
2025-10-21 13:11:58 +0300

    7. uzd

---
+++ alisa/js-formatīvais/function.js
@@ -0,0 +1,9 @@
+function intSequence(first, last){
+    for (let index = first; index < last+1; index++) {
+        console.log(index);
+    }
+}
+
+intSequence(5,8);
+intSequence(1,10);
+intSequence(7,16);
